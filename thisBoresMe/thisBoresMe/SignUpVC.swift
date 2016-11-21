6b2e8f8649591982466ce3eb1facc372684040a2
Emmet Susslin <esusslin@gmail.com>
2016-11-20 19:28:41 -0800

    day2

--- thisBoresMe/thisBoresMe/SignUpVC.swift
+++ thisBoresMe/thisBoresMe/SignUpVC.swift
@@ -7,42 +7,147 @@
 //
 
 import UIKit
+import Parse
 
-class SignUpVC: UIViewController {
+class signUpVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet weak var avaImg: UIImageView!
-    
+    @IBOutlet weak var avatarImage: UIImageView!
+
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
-    @IBOutlet weak var retypePassword: UITextField!
+    @IBOutlet weak var retypePasswordTxt: UITextField!
     
+    
+    @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var fullnameTxt: UITextField!
     @IBOutlet weak var bioTxt: UITextField!
     @IBOutlet weak var webTxt: UITextField!
-    
+
     
     @IBOutlet weak var scrollView: UIScrollView!
     
-    
-    var scrollViewHeeigh : CGFloat = 0
+    var scrollViewHeight : CGFloat = 0
     
     var keyboard = CGRect()
     
-    @IBOutlet weak var signUpBtn: UIButton!
-    
-    @IBOutlet weak var cancelBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // scrollview frame size
+        scrollView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+        scrollView.contentSize.height = self.view.frame.height
+        scrollViewHeight = scrollView.frame.size.height
 
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func signUpBtn_click(sender: AnyObject) {
-        print("sign up pressed")
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showKeyboard:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        // round avatar
+        avatarImage.layer.cornerRadius = avatarImage.frame.size.width / 2
+        avatarImage.clipsToBounds = true
+        
+        //declare hide keyboard tap
+        let hideTap = UITapGestureRecognizer(target: self, action: "hideKeyboardTap:")
+        hideTap.numberOfTapsRequired = 1
+        self.view.userInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+        
+        let avaTap = UITapGestureRecognizer(target: self, action: "loadImg:")
+        avaTap.numberOfTapsRequired = 1
+        avatarImage.userInteractionEnabled = true
+        avatarImage.addGestureRecognizer(avaTap)
+        
     }
     
-    @IBAction func cancelBtn_click(sender: AnyObject) {
+    func loadImg(recognizer:UITapGestureRecognizer) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        picker.allowsEditing = true
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        avatarImage.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // hide keyboard if tapped
+    func hideKeyboardTap(recoginizer:UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
+    // show keyboard
+    func showKeyboard(notification:NSNotification) {
+        keyboard = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey]!.CGRectValue)!
+        
+        UIView.animateWithDuration(0.4) { 
+            self.scrollView.frame.size.height = self.scrollViewHeight - self.keyboard.height
+        }
+    }
+    
+    // hide keyboard
+    func hideKeyboard(notification:NSNotification) {
+        UIView.animateWithDuration(0.4) { 
+            self.scrollView.frame.size.height = self.view.frame.height
+        }
+    }
+    
+    @IBAction func signUpButtonPressed(sender: AnyObject) {
+        print("sign up pressed")
+        
+        self.view.endEditing(true)
+        
+        if (usernameTxt.text!.isEmpty || passwordTxt.text!.isEmpty || retypePasswordTxt.text!.isEmpty || emailTxt.text!.isEmpty || fullnameTxt.text!.isEmpty || bioTxt.text!.isEmpty || webTxt.text!.isEmpty) {
+            
+            let alert = UIAlertController(title: "Please", message: "fill out all fields", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
+            alert.addAction(ok)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+        if passwordTxt.text != retypePasswordTxt.text {
+            let alert = UIAlertController(title: "PASSWORDS", message: "Do not match", preferredStyle: UIAlertControllerStyle.Alert)
+            let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
+            alert.addAction(ok)
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+        //send data to server to related columns
+        
+        let user = PFUser()
+        user.username = usernameTxt.text?.lowercaseString
+        user.email = emailTxt.text?.lowercaseString
+        user.password = passwordTxt.text
+        user["fullname"] = fullnameTxt.text?.lowercaseString
+        user["bio"] = bioTxt.text
+        user["web"] = webTxt.text?.lowercaseString
+        
+        // in edit profile
+        user["tel"] = ""
+        user["gender"] = ""
+        
+        let avatarData = UIImageJPEGRepresentation(avatarImage.image!, 0.5)
+        let avaFile = PFFile(name: "ava.jpg", data: avatarData!)
+        
+        user["ava"] = avaFile
+        
+        
+        user.signUpInBackgroundWithBlock { (success:Bool, error:NSError?) in
+            if success {
+                print("registered")
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
