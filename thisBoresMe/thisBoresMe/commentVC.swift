951943f052ac0f8fc9409716b8308c393eba09a5
Emmet Susslin <esusslin@gmail.com>
2016-12-03 20:44:16 -0800

    comment tableview code implemented

--- thisBoresMe/thisBoresMe/commentVC.swift
+++ thisBoresMe/thisBoresMe/commentVC.swift
@@ -12,13 +12,21 @@ import Parse
 var commentuuid = [String]()
 var commentowner = [String]()
 
-class commentVC: UIViewController {
+class commentVC: UIViewController, UITextViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     //UI objects
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var commentTxt: UITextView!
     @IBOutlet weak var sendBtn: UIButton!
     var refresh = UIRefreshControl()
+    
+    //arrays to hold data from server
+    
+    var usernameArray = [String]()
+    var avaArray = [PFFile]()
+    var commentArray = [String]()
+    var dateArray = [NSDate?]()
+    
     
     //values for reseting UI to default
     
@@ -38,6 +46,8 @@ class commentVC: UIViewController {
         
         //title at the top
         self.navigationItem.title = "COMMENTS"
+        
+        
         self.navigationItem.hidesBackButton = true
         let backBtn = UIBarButtonItem(title: "back", style: .Plain, target: self, action: #selector(commentVC.back(_:)))
         self.navigationItem.leftBarButtonItem = backBtn
@@ -127,6 +137,12 @@ class commentVC: UIViewController {
         
         sendBtn.frame = CGRectMake(commentTxt.frame.origin.x + commentTxt.frame.size.width + width / 32, commentTxt.frame.origin.y, width - (commentTxt.frame.origin.x + commentTxt.frame.size.width) - (width / 32) * 2, commentTxt.frame.size.height)
         
+        //delegates
+        commentTxt.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         // assign reseting values
         
         tableViewHeight = tableView.frame.size.height
@@ -135,21 +151,112 @@ class commentVC: UIViewController {
         
     }
     
-    func back(sender : UIBarButtonItem) {
+    //while writing
+    
+    func textViewDidChange(textView: UITextView) {
+        // disable button if entered no text
         
-        //push back
-        self.navigationController?.popViewControllerAnimated(true)
+        let spacing = NSCharacterSet.whitespaceAndNewlineCharacterSet()
         
-        // clean comment uui from last holding information
-        if !commentuuid.isEmpty {
-            commentuuid.removeLast()
+        if commentTxt.text.stringByTrimmingCharactersInSet(spacing).isEmpty {
+            sendBtn.enabled = true
+        } else {
+            sendBtn.enabled = false
+        }
         
-        //clean comment uui from last holding information
-        if !commentowner.isEmpty {
-            commentowner.removeLast()
-
+        // + paragaph
+        if textView.contentSize.height > textView.frame.size.height && textView.frame.height < 130 {
+            
+            let difference = textView.contentSize.height - textView.frame.size.height
+            
+            textView.frame.origin.y = textView.frame.origin.y - difference
+            textView.frame.size.height = textView.contentSize.height
+            
+            if textView.contentSize.height + keyboard.height + commentY >= tableView.frame.size.height {
+                tableView.frame.size.height = tableView.frame.size.height - difference
+            }
+        }
+        
+        else if textView.contentSize.height < textView.frame.size.height {
+            
+            // find difference to deduct
+            let difference = textView.frame.size.height - textView.contentSize.height
+            
+            textView.frame.origin.y = textView.frame.origin.y + difference
+            textView.frame.size.height = textView.contentSize.height
+            
+            if textView.contentSize.height + keyboard.height + commentY > tableView.frame.size.height {
+                tableView.frame.size.height = tableView.frame.size.height + difference
+            }
         }
     }
-
+    
+    // TABLEVIEW
+    // cell numb
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commentArray.count
     }
+    
+    // cell height
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    // cell config
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // declare cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! commentCell
+        
+        cell.usernameBtn.setTitle(usernameArray[indexPath.row], forState: .Normal)
+        cell.usernameBtn.sizeToFit()
+        cell.commentLbl.text = commentArray[indexPath.row]
+        avaArray[indexPath.row].getDataInBackgroundWithBlock { (data:NSData?, error:NSError?) -> Void in
+            cell.avaImg.image = UIImage(data: data!)
+        }
+        
+        // calculate date
+        let from = dateArray[indexPath.row]
+        let now = NSDate()
+        let components : NSCalendarUnit = [.Second, .Minute, .Hour, .Day, .WeekOfMonth]
+        let difference = NSCalendar.currentCalendar().components(components, fromDate: from!, toDate: now, options: [])
+        
+        if difference.second <= 0 {
+            cell.dateLbl.text = "now"
+        }
+        if difference.second > 0 && difference.minute == 0 {
+            cell.dateLbl.text = "\(difference.second)s."
+        }
+        if difference.minute > 0 && difference.hour == 0 {
+            cell.dateLbl.text = "\(difference.minute)m."
+        }
+        if difference.hour > 0 && difference.day == 0 {
+            cell.dateLbl.text = "\(difference.hour)h."
+        }
+        if difference.day > 0 && difference.weekOfMonth == 0 {
+            cell.dateLbl.text = "\(difference.day)d."
+        }
+        if difference.weekOfMonth > 0 {
+            cell.dateLbl.text = "\(difference.weekOfMonth)w."
+        }
+        
+        return cell
+    }
+    
+        // go back
+        func back(sender : UIBarButtonItem) {
+            
+            // push back
+            self.navigationController?.popViewControllerAnimated(true)
+            
+            // clean comment uui from last holding infromation
+            if !commentuuid.isEmpty {
+                commentuuid.removeLast()
+            }
+            
+            // clean comment owner from last holding infromation
+            if !commentowner.isEmpty {
+                commentowner.removeLast()
+            }
+        }
 }
